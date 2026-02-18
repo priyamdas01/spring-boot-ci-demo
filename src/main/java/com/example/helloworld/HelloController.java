04427cd8d21f3a5ca6ff25192e6eb234247c8088
priyamdas01 <12priyamdas34@gmail.com>
2026-02-18 03:23:20 -0500

    updated deploy file

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from cicd pipleine...!";
+        return "Hello from cicd pipleine...";
     }
 }
